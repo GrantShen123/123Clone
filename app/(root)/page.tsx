37dd939cc7a8502b30f19f9af227df106e60ee27
Grant Shen <gshen@Grants-MBP.hsd1.ca.comcast.net>
2024-06-22 12:39:12 -0700

    new commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import HeaderBox from '../../components/HeaderBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Grant' };
   return (
-    <div>Home</div>
+    <section className = "Home">
+      <div className="hone-content">
+        <header className = "home-header">
+          <HeaderBox 
+            type = "greeting"
+            title = "Welcome to the app"
+            user = {loggedIn?.firstName ||'guest'}  
+            subtext = "Access and manage your account and transactions efficiently." 
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
